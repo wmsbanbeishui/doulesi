907d880102e26c1fddbe2db8043aadbbe96d4fb8
wangmengsi <wangmengsi@namaiche.com>
2019-08-23 12:11:41 +0800

    YII2

---
+++ common/config/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@pc', dirname(dirname(__DIR__)).'/pc');
+Yii::setAlias('@admin', dirname(dirname(__DIR__)).'/admin');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
