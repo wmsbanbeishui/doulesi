4fd3a78eabbc06439c7d0622311df3283bfd438c
wangmengsi <wangmengsi@namaiche.com>
2019-10-10 11:32:58 +0800

    邮件

--- common/config/params.php
+++ common/config/params.php
@@ -3,4 +3,15 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+
+
+
+
+	// 开启发送错误到邮箱 0-关闭；1-开启；
+	'send_error_report' => 0,
+	// 错误报告邮箱接收人
+	'error_report_receiver' =>  [
+		'773728950@qq.com' => ['admin'],
+		'269243737@qq.com' => ['admin'],
+	],
 ];
