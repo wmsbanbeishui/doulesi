361cb0dd68faa55de8588c4010dd7f76ce8ef3c7
banbeishui <269243737@qq.com>
2020-07-29 19:01:41 +0800

    swooleAdd

--- admin/controllers/TestController.php
+++ admin/controllers/TestController.php
@@ -1,37 +1,54 @@
 <?php
+
 namespace admin\controllers;
 
 use admin\models\form\TestImportForm;
 use admin\controllers\base\BaseController;
+use common\models\table\WorkLog;
 use Yii;
 
 class TestController extends BaseController
 {
-	public function actionIndex() {
-		return $this->render('index');
-	}
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
 
-	public function actionImport(){
-		$request = Yii::$app->getRequest();
+    public function actionImport()
+    {
+        $request = Yii::$app->getRequest();
 
-		$form = new TestImportForm();
+        $form = new TestImportForm();
 
-		if ($request->getIsPost()) {
-			if ($form->import()) {
-				return $this->redirect('index');
-			}
-		} else {
-			return $this->render('import', ['model' => $form]);
-		}
-	}
+        if ($request->getIsPost()) {
+            if ($form->import()) {
+                return $this->redirect('index');
+            }
+        } else {
+            return $this->render('import', ['model' => $form]);
+        }
+    }
 
-	public function actionImportTemplate()
-	{
-		$response = Yii::$app->getResponse();
-		return $response->sendFile(Yii::getAlias('@webroot/import_template.xlsx'));
-	}
+    public function actionImportTemplate()
+    {
+        $response = Yii::$app->getResponse();
+        return $response->sendFile(Yii::getAlias('@webroot/import_template.xlsx'));
+    }
 
-	public function actionTest() {
-	    echo '222';exit;
+    public function actionTest()
+    {
+        self::swooleAdd();
+        echo '111';
+    }
+
+    public function swooleAdd()
+    {
+        swoole_timer_after(1000, function () {
+            $work = new WorkLog();
+            $work->plan = '计划';
+            $work->finish = '完成';
+            $work->date = date('Y-m-d');
+            $work->save();
+        });
     }
 }
