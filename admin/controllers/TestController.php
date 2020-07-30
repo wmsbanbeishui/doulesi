a6eb831ed53b8afea99ddd0098ec62cd9e418a5a
banbeishui <269243737@qq.com>
2020-07-30 10:14:05 +0800

    test

--- admin/controllers/TestController.php
+++ admin/controllers/TestController.php
@@ -40,7 +40,8 @@ class TestController extends BaseController
     {
         $finish = '已完成123';
         echo '111';
-        pclose(popen('php yii test/swoole-add -finish ' . $finish . ' &', 'w'));
+        //pclose(popen('php yii test/swoole-add -finish ' . $finish . ' &', 'w'));
+        pclose(popen('php ../web/cli2cgi.php -finish ' . $finish . ' &', 'w'));
         echo '222';
     }
 }
