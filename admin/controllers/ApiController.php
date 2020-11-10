68619c93178c54805bbdf7e1bb1d56c8802792a2
banbeishui <269243737@qq.com>
2020-11-10 18:24:03 +0800

    支付宝支付沙箱测试

--- admin/controllers/ApiController.php
+++ admin/controllers/ApiController.php
@@ -3,8 +3,11 @@
 namespace admin\controllers;
 
 use admin\controllers\base\AuthController;
+use common\helpers\Helper;
 use common\models\table\ArticleSearch;
 use common\models\table\Category;
+use common\alipay\aop\request\AlipayTradePagePayRequest;
+use common\models\table\CodeMsg;
 use Yii;
 
 class ApiController extends AuthController
@@ -51,5 +54,74 @@ class ApiController extends AuthController
 
 
         return ['code' => 0, 'data' => $data];
+    }
+
+    public function actionAliPay()
+    {
+        Yii::$app->response->format = 'json';
+        require_once Yii::getAlias('@common/alipay/aop/request/AlipayTradePagePayRequest.php');
+        require_once Yii::getAlias('@common/alipay/aop/AopClient.php');
+        $config = Helper::getParam('alipay');
+
+        $aop = new \AopClient();
+        $aop->gatewayUrl = $config['gatewayUrl'];
+        $aop->appId = $config['app_id'];
+        $aop->rsaPrivateKey = $config['merchant_private_key'];
+        $aop->alipayrsaPublicKey = $config['alipay_public_key'];
+        $aop->signType = $config['sign_type'];
+
+        $request = new \AlipayTradePagePayRequest();
+        $request->setNotifyUrl($config['notify_url']);
+        $request->setReturnUrl($config['return_url']);
+
+        $pay_data = [
+            'out_trade_no' => Helper::gen_order_no(),
+            'product_code' => $config['product_code'],
+            'total_amount' => '0.1',
+            'subject' => '商品1',
+            'body' => '特蓝图'
+        ];
+
+        $pay_data = json_encode($pay_data);
+        $request->setBizContent($pay_data);
+        $result = $aop->execute($request);
+
+        $responseNode = str_replace(".", "_", $request->getApiMethodName()) . "_response";
+        $resultCode = $result->$responseNode->code;
+        if(!empty($resultCode)&&$resultCode == 10000){
+            return [
+                'code' => 0,
+                'msg' => '操作成功'
+            ];
+        } else {
+            return [
+                'code' => 101,
+                'msg' => '操作失败'
+            ];
+        }
+    }
+
+    public function actionAliPayNotify()
+    {
+        $data = Yii::$app->request->post();
+        $config = Helper::getParam('alipay');
+        $service_obj = new \AlipayTradeService($config);
+        $result = $service_obj->check($data);
+
+        if ($result) {
+            if ($data['trade_status'] === 'TRADE_SUCCESS') {
+                $model = new CodeMsg();
+                $model->mobile = '17322350852';
+                $model->code = '2222';
+                $model->type = 3;
+                $model->deadline = time() + 120;
+                $model->save();
+
+                echo 'success';
+            }
+        } else {
+            echo '非法操作';
+        }
+
     }
 }
