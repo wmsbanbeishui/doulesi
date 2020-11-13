682792ed86332d09f7882ecdd9c60ba236effa96
banbeishui <269243737@qq.com>
2020-11-13 11:18:17 +0800

    save

--- admin/controllers/TestController.php
+++ admin/controllers/TestController.php
@@ -122,6 +122,7 @@ class TestController extends ApiController
     public function actionAliPayCode()
     {
         header("Content-type: text/html; charset=utf-8");
+        require_once Yii::getAlias('@common/phpqrcode/phpqrcode.php');
         require_once Yii::getAlias('@common/alipay/pcweb/aop/request/AlipayTradePrecreateRequest.php');
         require_once Yii::getAlias('@common/alipay/pcweb/aop/AopClient.php');
         $config = Helper::getParam('alipay');
@@ -158,10 +159,9 @@ class TestController extends ApiController
         $resultCode = $result->$responseNode->code;
         if(!empty($resultCode) && $resultCode == 10000){
 
-            $http_server = Helper::get_request_host();
             $qr_code_url = $result->$responseNode->qr_code;
 
-            $qr_code_url = $http_server.'/test/qrcode?data='.$qr_code_url;
+            $qr_code_url = \QRcode::png($qr_code_url);
             return [
                 'code' => 0,
                 'msg' => '',
